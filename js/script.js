ce226606a4299b860febca739826630d1a383492
logic23dev <logic23dev@gmail.com>
2024-11-15 01:54:30 +0200

    fix data.json url

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-fetch('../data.json')
+fetch('data.json')
   .then(response => response.json())
   .then(data => fillComponent(data))
   .catch(error => console.error('Unable to fetch data:', error));
